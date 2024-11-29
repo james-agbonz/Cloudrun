f1a13b2a898d2d3522b2c951109b0c1438523aee
james-agbonz <153930162+james-agbonz@users.noreply.github.com>
2024-11-29 01:05:42 -0500

    Update crun.py

--- crun.py
+++ crun.py
@@ -1,10 +1,5 @@
-from flask import Flask, jsonify
+@app.route("/", methods=['GET', 'POST'])
+def hello_world():
+    """Example Hello World route."""
 
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return jsonify({'message': 'Hello from Python!'})
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    return f"Hello World!!!@@@!!!"
